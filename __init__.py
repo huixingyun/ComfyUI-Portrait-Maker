85464716eedd252bc2c68d0a10cb4fc3141093fe
warmdust <53068584+warmdust@users.noreply.github.com>
2025-01-21 16:38:34 +0800

    delete auto download models when comfyui start

--- __init__.py
+++ __init__.py
@@ -11,27 +11,6 @@ from portrait.nodes import *
 # import pydevd_pycharm
 # pydevd_pycharm.settrace('49.7.62.197', port=10090, stdoutToServer=True, stderrToServer=True)
 
-def urldownload_progressbar(url, file_path):
-    response = requests.get(url, stream=True)
-    total_size = int(response.headers.get('content-length', 0))
-    progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
-    with open(file_path, 'wb') as f:
-        for chunk in response.iter_content(1024):
-            if chunk:
-                f.write(chunk)
-                progress_bar.update(len(chunk))
-
-    progress_bar.close()
-
-print("Start Setting weights")
-for url, filename in zip(urls, filenames):
-    if os.path.exists(filename):
-        continue
-    print(f"Start Downloading: {url} Download To {filename}")
-    print(f"开始下载: {url} 下载到 {filename}")
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    urldownload_progressbar(url, filename)
-
 NODE_CLASS_MAPPINGS = {
     "PM_RetinaFace": RetinaFacePM,
     "PM_FaceFusion": FaceFusionPM,
